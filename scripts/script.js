1e6e691908d64809a568389e31be1f57fbe43dc7
migueJous <migue.280.999@gmail.com>
2023-03-14 23:38:57 -0500

    funciones v1 modificadas

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,6 @@
 let contenedor = document.getElementById("contenedor")
 
-window.onload(() => {
-    
-    let nombre  = prompt("ingresa tu nombre: ")
-    contenedor.innerHTML = `Vienvenido a la practica ${nombre}`
-
-})
-
-
 const boton = document.getElementById("botonN1")
     .addEventListener("click", () => console.log("mensaje por consola!!"))
 
-    
+
