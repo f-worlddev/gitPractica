05b967e1c2acc2d10295182c338b3dbee009c968
migueJous <migue.280.999@gmail.com>
2023-03-14 23:40:14 -0500

    actualizacion v1.5 :v

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,3 @@
-let contenedor = document.getElementById("contenedor")
-
 const boton = document.getElementById("botonN1")
     .addEventListener("click", () => console.log("mensaje por consola!!"))
 
